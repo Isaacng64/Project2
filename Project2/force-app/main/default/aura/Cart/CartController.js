10d76a2f0c55e058c52c0c1b7b75850a83412ff3
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-09-22 07:16:20 -0700

    trying datatable

--- Project2/force-app/main/default/aura/Cart/CartController.js
+++ Project2/force-app/main/default/aura/Cart/CartController.js
@@ -7,6 +7,32 @@
     },
     sameShipping : function(component, event, helper) {
         component.set("v.bSameShipping", ! component.get("v.bSameShipping"));
+    },
+    init: function (component, event, helper) {
+        component.set('v.columns', [
+            {label: 'Opportunity name', fieldName: 'opportunityName', type: 'text'},
+            {label: 'Account name', fieldName: 'accountName', type: 'text'},
+            {label: 'Close date', fieldName: 'closeDate', type: 'date'},
+            {label: 'Confidence', fieldName: 'confidence', type: 'percent'},
+            {label: 'Amount', fieldName: 'amount', type: 'currency', typeAttributes: { currencyCode: 'EUR'}},
+            {label: 'Contact Email', fieldName: 'contact', type: 'email'},
+            {label: 'Contact Phone', fieldName: 'phone', type: 'phone'},
+            {label: 'Website', fieldName: 'website', type: 'url', typeAttributes: { target: '_parent'}},
+            {label: 'Address', fieldName: 'address', type: 'location'}
+        ]);
+
+        var fetchData = {
+            opportunityName: "company.companyName",
+            accountName : "name.findName",
+            closeDate : "date.future",
+            amount : "finance.amount",
+            contact: "internet.email",
+            phone : "phone.phoneNumber",
+            website : "internet.url"
+        };
+
+        component.set("v.data", fetchData);
+        //helper.fetchData(cmp, fetchData, 100);
     }
 })
 
