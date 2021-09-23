0d2fde8e39570238966350afd7cde285204a6b34
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-09-23 08:46:22 -0700

    all functionality refactored, save cart table

--- Project2/force-app/main/default/aura/PaymentShippingForm/PaymentShippingFormController.js
+++ Project2/force-app/main/default/aura/PaymentShippingForm/PaymentShippingFormController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
+    emitCartConfirm : function(component, event, helper) {
+        let evt = component.getEvent("ToggleCart");
+        evt.fire();
+    },
+    sameShipping : function(component, event, helper) {
+        component.set("v.bSameShipping", ! component.get("v.bSameShipping")); // client side
 
+        helper.setSameShipping(component, event, helper, component.get("v.bSameShipping")); // update on order draft
+    },
+    getPersistentSameShipping : function(component, event, helper) {
+        helper.getPersistentSameShipping(component, event);
     }
 })
--- Project2/force-app/main/default/aura/PaymentShippingForm/PaymentShippingFormHelper.js
+++ Project2/force-app/main/default/aura/PaymentShippingForm/PaymentShippingFormHelper.js
@@ -1,5 +1,42 @@
 ({
-    helperMethod : function() {
+    getPersistentSameShipping : function(component, event) {
+        let a = component.get("c.getSameAddress");
 
+        a.setParams({"orderID" : "8015f000000g3ZeAAI"});         //component.get("draftOrderID")});
+
+        a.setCallback(this, function(response){
+            if(response.getState() == "SUCCESS"){
+                
+                component.set("v.bSameShipping", response.getReturnValue());
+
+                //return response.getReturnValue();
+
+            }else{
+                //return false;
+            }
+
+            component.set("v.bResponseReceived", true);
+        });
+
+        $A.enqueueAction(a);
+    },
+    setSameShipping : function(component, event, helper, bool) {
+        let a = component.get("c.setSameAddress");
+
+        a.setParams({"orderID" : "8015f000000g3ZeAAI", "bool" : bool});         //component.get("draftOrderID")});
+
+        a.setCallback(this, function(response){
+            if(response.getState() == "SUCCESS"){
+                
+                //component.set("v.bSameShipping", response.getReturnValue());
+
+                //return response.getReturnValue();
+
+            }else{
+                //return false;
+            }
+        });
+
+        $A.enqueueAction(a);
     }
 })
