851e13ccc84a6430f4a46418bbccc0b302876814
Vicky P <aperezus2000@gmail.com>
2021-09-27 14:54:39 -0400

    New Objects

---
+++ force-app/main/default/aura/mailingList/mailingListController.js
@@ -0,0 +1,8 @@
+({
+    Initialize: function (component, event, helper) {
+        helper.DoInit(component);
+    },
+    ClearForm: function(component, event, helper) { 
+  		helper.Clear(component);      
+    }
+})
